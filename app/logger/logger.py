f62bb05c0a83fc5d1075e22854d5a3d8add8b557
Lucas <uzanlah@gmail.com>
2025-01-06 15:34:33 +0100

    Update .gitignore, docker-compose, and logging configuration; add model files.
    - Added 'static/' and 'logs/' to .gitignore.
    - Updated volume paths in docker-compose.yml to reflect new directory structure.
    - Enhanced logger in logger.py to create a dated log file in the 'logs' directory.
    - Removed commented-out check for changes in ai_models from deploy.yml.
    - Added new model files: efficientnet_b0_disease_best.pth and efficientnet_b0_identification_best.pth.

--- app/logger/logger.py
+++ app/logger/logger.py
@@ -1,13 +1,24 @@
 import logging
+from datetime import datetime
+import os
 
+# Créer le dossier logs s'il n'existe pas
+log_directory = "../logs"
+if not os.path.exists(log_directory):
+    os.makedirs(log_directory)
+
+# Définir le nom du fichier de log avec la date
+log_filename = os.path.join(log_directory, f"pyto-{datetime.now().strftime('%Y-%m-%d')}.log")
+
+# Configuration du logger
 logging.basicConfig(
-    level=logging.INFO,  # Log level
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",  # Log format
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     handlers=[
-        logging.FileHandler("app.log"),  # Log to file
-        logging.StreamHandler()  # Optional: Also log to console
+        logging.FileHandler(log_filename),  # Log dans un fichier daté
+        logging.StreamHandler()  # Log aussi dans la console
     ]
 )
 
-# Create a logger
+# Créer le logger
 logger = logging.getLogger("Pyto API")
