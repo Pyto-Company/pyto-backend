feef54b92f61d33e34b6abfcdf5238f25aba3ec3
Lucas <uzanlah@gmail.com>
2025-01-11 17:12:23 +0100

    Update docker-compose.yml to add app-network for fastapi service and clean up meteo.py by removing unnecessary newline
    - Added 'app-network' to the fastapi service in docker-compose.yml to improve network configuration.
    - Removed an unnecessary newline in meteo.py for cleaner code.
    
    This commit enhances the service configuration and improves code readability.

--- app/service/meteo.py
+++ app/service/meteo.py
@@ -8,4 +8,3 @@ class MeteoService():
         result = requests.get('https://api.tomorrow.io/v4/weather/realtime?location={location}&apikey=JUx0lyHFlnwuu95W5PBsGDbu15xqQZYq').json()
         weatherData = WeatherReport(**result) 
         return weatherData
-
