968acdd35065d06d9fb8a2c0333a036e0fe45ff3
Lucas <uzanlah@gmail.com>
2025-01-12 19:50:18 +0100

    Refactor docker-compose configuration and deployment workflow
    - Updated docker-compose.yml to rename 'postgres' service to 'postgresql' and added health checks for both fastapi and postgresql services to ensure proper startup sequencing.
    - Introduced a link from fastapi to postgresql for improved service communication.
    - Modified the deployment workflow in deploy.yml to remove the docker-compose.yml file after deployment, streamlining the process.
    - Cleaned up meteo.py by removing an unnecessary newline for better code readability.
    
    These changes enhance service dependencies, improve deployment reliability, and maintain code cleanliness.

--- app/service/meteo.py
+++ app/service/meteo.py
@@ -8,3 +8,4 @@ class MeteoService():
         result = requests.get('https://api.tomorrow.io/v4/weather/realtime?location={location}&apikey=JUx0lyHFlnwuu95W5PBsGDbu15xqQZYq').json()
         weatherData = WeatherReport(**result) 
         return weatherData
+
