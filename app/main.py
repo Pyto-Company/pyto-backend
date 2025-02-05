ac11356a020edfb3c48e6d7507a6bd817f298cd0
Lucas <uzanlah@gmail.com>
2025-02-05 21:41:17 +0100

    Ensure Firebase configuration uses string type for environment variables
    Cast Firebase-related environment variables to strings in both docker-compose.yml and main.py to prevent potential type-related issues and ensure consistent configuration handling.

--- app/main.py
+++ app/main.py
@@ -110,11 +110,11 @@ app.openapi = custom_openapi
 
 firebase_config = {
   "type": "service_account",
-  "project_id": os.getenv("FIREBASE_PROJECT_ID"),
-  "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
-  "private_key": os.getenv("FIREBASE_PRIVATE_KEY"),
-  "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
-  "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+  "project_id": str(os.getenv("FIREBASE_PROJECT_ID")),
+  "private_key_id": str(os.getenv("FIREBASE_PRIVATE_KEY_ID")),
+  "private_key": str(os.getenv("FIREBASE_PRIVATE_KEY")),
+  "client_email": str(os.getenv("FIREBASE_CLIENT_EMAIL")),
+  "client_id": str(os.getenv("FIREBASE_CLIENT_ID")),
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
   "token_uri": "https://oauth2.googleapis.com/token",
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
